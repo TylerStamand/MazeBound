f225960bc3557117dba205f4a68830c4f3c21b79
Tyler St. Amand <stamandtyler@gmail.com>
2023-02-01 08:40:14 -0500

    Included Input System and setup basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
 
+
+    [SerializeField] float moveSpeed;
+
+    new Rigidbody2D rigidbody;
+    Vector2 inputVector;
+    void Awake() {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context) {
+        inputVector = context.ReadValue<Vector2>();
+
+    }
+
     void Update() {
+        rigidbody.velocity = inputVector * moveSpeed;
 
     }
 }
