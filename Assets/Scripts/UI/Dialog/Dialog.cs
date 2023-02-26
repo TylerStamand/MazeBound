799c83646cbd98f254ee58f9c53fad479a9a1fed
Tyler St. Amand <stamandtyler@gmail.com>
2023-02-26 15:23:34 -0500

    Rename dungeon1 folder to dungeon2, fixed dialog choice

--- Assets/Scripts/UI/Dialog/Dialog.cs
+++ Assets/Scripts/UI/Dialog/Dialog.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Dialog", menuName = "ScriptableObjects/Dialog")]
 
 public class Dialog : ScriptableObject {
-    public bool IsChoice { get; private set; }
 
     [TextArea(5, 5)]
     public List<string> Sentences;
 
+    [field: SerializeField] public bool IsChoice { get; private set; }
+    [TextArea(5, 5)]
     public List<string> YesDialog;
+    [TextArea(5, 5)]
     public List<string> NoDialog;
 
 }
