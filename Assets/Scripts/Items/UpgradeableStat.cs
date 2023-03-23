24447dd5fbb30e9e127c8c35f8dc7322f8e7fe02
Tyler <stamandtyler@gmail.com>
2023-03-23 15:31:27 -0400

    Fixed Stats not loading

--- Assets/Scripts/Items/UpgradeableStat.cs
+++ Assets/Scripts/Items/UpgradeableStat.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Newtonsoft.Json;
 
 public class UpgradeableStat {
     public static int MaxLevel = 5;
@@ -16,6 +17,18 @@ public class UpgradeableStat {
     public float UpgradeValueMultiplier { get; private set; }
 
     public event Action OnUpgrade;
+
+    [JsonConstructor]
+    public UpgradeableStat(string name, int level, float baseValue, float currentValue, int upgradeCost, int upgradeCostBase, float upgradeCostMultiplier, float upgradeValueMultiplier) {
+        Name = name;
+        Level = level;
+        BaseValue = baseValue;
+        CurrentValue = currentValue;
+        UpgradeCost = upgradeCost;
+        UpgradeCostBase = upgradeCostBase;
+        UpgradeCostMultiplier = upgradeCostMultiplier;
+        UpgradeValueMultiplier = upgradeValueMultiplier;
+    }
 
     public UpgradeableStat(string name, float baseValue, int upgradeCostBase, float upgradeValueMultiplier, float upgradeCostMultiplier) {
         Name = name;
